0c5791099dc3009c45b695afa1dc62761ca35103
NJonsey <nyejones@hotmail.co.uk>
2015-11-27 13:02:25 +0000

    changes made

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 $( document ).ready(function() {
 
-  $('#search').keyup(function(){
-    var searchField = $('#search').val();
-    console.log(searchField);
-    var myExp = new RegExp(searchField, 'i');
-    $.getJSON('http://localhost:3000/api/homes', function(data){
-      var output = '<ul class="searchresult">';
-      $.each(data.homes, function(i, home){
-        if((home.address.search(myExp) != -1) || (home.postcode.search(myExp) != -1)) {
-          output +='<li>';
-          output +='<p>' + home.address + '</p>';
-          // output +='<img src="images/' + val.shortname + '_tn.jpg" alt="'+ val.name +'" />';
-          output +='<p>' + home.postcode + '</p>';
-          output +='</li>';
-        }
-      });
-      output += '</ul>';
-      $('#update').append(output);
+$('#search').keyup(function(){
+  var searchField = $('#search').val();
+  var myExp = new RegExp(searchField, 'i');
+  $.getJSON('data.json', function(data){
+    console.log(data);
+    var output = '<ul class="searchresult">';
+    $.each(data, function(key, val) {
+      console.log(val);
+      if((val.address.search(myExp) != -1) || (val.poscode.search(myExp) != -1)) {
+        output +='<li>';
+        output +='<p>' + val.address + '</p>';
+        output +='<p>' + val.postcode + '</p>';
+        output +='</li>';
+      }
     });
+    output += '</ul>';
+    $('#update').append(output);
   });
-
-
 });
+})
+
+
+
